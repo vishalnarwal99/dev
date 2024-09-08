cdca93f9924247f3e1f53f6c245b9b70dd055b4e
Vishal Narwal <32016029+vishalnarwal99@users.noreply.github.com>
2024-09-08 03:47:18 +0000

    Refined: Water And Jug Problem

--- Water And Jug Problem.py
+++ Water And Jug Problem.py
@@ -1,57 +1,61 @@
 from queue import Queue
 
 
-def breadth_first_search(initial_volumes: tuple[int], target_volume: int, capacities: tuple[int]) -> None:
+def breadth_first_search(initial: tuple[int], target: int, capacity: tuple[int]) -> None:
     front: Queue[tuple[int]] = Queue()
-    front.put(initial_volumes)
     visited: set[tuple[int]] = set()
 
-    while not front.empty():
-        current_volumes = front.get()
+    front.put(initial)
+    visited.add(initial)
 
-        if current_volumes[0] == target_volume or current_volumes[1] == target_volume:
+    while not front.empty():
+        current = front.get()
+
+        if current[0] == target or current[1] == target:
             print('Found target volume.')
             return
 
-        if current_volumes[0] < capacities[0] and ((capacities[0], current_volumes[1]) not in visited):
-            front.put((capacities[0], current_volumes[1]))
-            visited.add((capacities[0], current_volumes[1]))
+        if current[0] < capacity[0] and ((capacity[0], current[1]) not in visited):
+            front.put((capacity[0], current[1]))
+            visited.add((capacity[0], current[1]))
 
-        if current_volumes[1] < capacities[1] and ((current_volumes[0], capacities[1]) not in visited):
-            front.put((current_volumes[0], capacities[1]))
-            visited.add((current_volumes[0], capacities[1]))
+        if current[1] < capacity[1] and ((current[0], capacity[1]) not in visited):
+            front.put((current[0], capacity[1]))
+            visited.add((current[0], capacity[1]))
 
-        if current_volumes[0] > 0 and ((0, current_volumes[1]) not in visited):
-            front.put((0, current_volumes[1]))
-            visited.add((0, current_volumes[1]))
+        if current[0] > 0 and ((0, current[1]) not in visited):
+            front.put((0, current[1]))
+            visited.add((0, current[1]))
 
-        if current_volumes[1] > 0 and ((current_volumes[0], 0) not in visited):
-            front.put((current_volumes[0], 0))
-            visited.add((current_volumes[0], 0))
+        if current[1] > 0 and ((current[0], 0) not in visited):
+            front.put((current[0], 0))
+            visited.add((current[0], 0))
 
-        if current_volumes[0] > 0 and ((max(0, current_volumes[0] + current_volumes[1] - capacities[1]),
-                                        min(current_volumes[0] + current_volumes[1], capacities[1])) not in visited):
-            front.put((max(0, current_volumes[0] + current_volumes[1] - capacities[1]),
-                       min(current_volumes[0] + current_volumes[1], capacities[1])))
-            visited.add((max(0, current_volumes[0] + current_volumes[1] - capacities[1]),
-                         min(current_volumes[0] + current_volumes[1], capacities[1])))
+        if current[0] > 0 and ((max(0, current[0] + current[1] - capacity[1]),
+                                min(current[0] + current[1], capacity[1])) not in visited):
+            front.put((max(0, current[0] + current[1] - capacity[1]),
+                       min(current[0] + current[1], capacity[1])))
+            visited.add((max(0, current[0] + current[1] - capacity[1]),
+                         min(current[0] + current[1], capacity[1])))
             
-        if current_volumes[1] > 0 and ((min(current_volumes[0] + current_volumes[1], capacities[0]),
-                                        max(0, current_volumes[0] + current_volumes[1] - capacities[0])) not in visited):
-            front.put((min(current_volumes[0] + current_volumes[1], capacities[0]),
-                       max(0, current_volumes[0] + current_volumes[1] - capacities[0])))
-            visited.add((min(current_volumes[0] + current_volumes[1], capacities[0]),
-                         max(0, current_volumes[0] + current_volumes[1] - capacities[0])))
+        if current[1] > 0 and ((min(current[0] + current[1], capacity[0]),
+                                max(0, current[0] + current[1] - capacity[0])) not in visited):
+            front.put((min(current[0] + current[1], capacity[0]),
+                       max(0, current[0] + current[1] - capacity[0])))
+            visited.add((min(current[0] + current[1], capacity[0]),
+                         max(0, current[0] + current[1] - capacity[0])))
+            
+        print(front.queue)
             
     print('Target volume not found.')
 
 
 if __name__ == '__main__':
-    capacities: tuple[int] = (int(input('Enter jug 1 capacity: ')),)
-    capacities = capacities + (int(input('Enter jug 2 capacity: ')),)
+    capacity: tuple[int] = (int(input('Enter jug 1 capacity: ')),)
+    capacity = capacity + (int(input('Enter jug 2 capacity: ')),)
 
-    target_volume: int = int(input('Enter target volume: '))
-    initial_volumes = (0, 0)
+    target: int = int(input('Enter target volume: '))
+    initial = (0, 0)
 
-    breadth_first_search(initial_volumes, target_volume, capacities)
+    breadth_first_search(initial, target, capacity)
 
